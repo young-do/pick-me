4f4ad781c97fd3823eaba8cdaa9a23fb635765a5
young-do <yd199323@gmail.com>
2023-06-16 20:50:42 +0900

    tsv 파일 기반 팀원 선택

---
+++ src/pages/Game.tsx
@@ -0,0 +1,279 @@
+import { useMemo, useState } from "react";
+import { Team, User, useGame } from "../store/useGame";
+import {
+  Box,
+  Button,
+  HStack,
+  Select,
+  VStack,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
+
+// 각 라운드마다 팀이 돌아가면서 유저를 0~n명 선택할 수 있다.
+export const GamePage = () => {
+  const {
+    users,
+    positions,
+    teams,
+    maxRound,
+    selectedPosition,
+    selectedJoinedTeam,
+    selectedRound,
+    selectedTeamId,
+    setSelectedPosition,
+    setSelectedJoinedTeam,
+    setSelectedRound,
+    setSelectedTeamId,
+    pick,
+    unpick,
+  } = useGame();
+
+  const filteredUsers = useMemo(
+    () =>
+      users
+        .filter((user) => {
+          if (selectedPosition === "") return true;
+          return user.position.includes(selectedPosition);
+        })
+        .filter((user) => {
+          if (selectedJoinedTeam === "selected")
+            return user.joined_team_id !== null;
+          if (selectedJoinedTeam === "not-selected")
+            return user.joined_team_id === null;
+          return true;
+        })
+        .filter((user) => {
+          if (selectedRound === -1) return true;
+          return user.choices[selectedRound] === selectedTeamId;
+        }),
+    [selectedJoinedTeam, selectedPosition, selectedRound, selectedTeamId, users]
+  );
+
+  const teamMembers = useMemo(() => {
+    return users.filter((user) => user.joined_team_id === selectedTeamId);
+  }, [selectedTeamId, users]);
+
+  const allMemberByTeam = useMemo(() => {
+    const allMemberByTeam: Record<Team["id"], User[]> = {};
+    teams.forEach((team) => {
+      allMemberByTeam[team.id] = users.filter(
+        (user) => user.joined_team_id === team.id
+      );
+    });
+    allMemberByTeam["아직 속하지 않은 사람들"] = users.filter(
+      (user) => user.joined_team_id === null
+    );
+
+    return Object.entries(allMemberByTeam);
+  }, [teams, users]);
+
+  return (
+    <>
+      <SetupBox />
+
+      <HStack width={"100vw"} minHeight={"100vh"} alignItems={"flex-start"}>
+        <Box flex="1" padding={2}>
+          <HStack>
+            <Select
+              value={selectedPosition}
+              onChange={(e) => setSelectedPosition(e.target.value)}
+            >
+              <option value={""}>모든 포지션</option>
+              {positions.map((position) => (
+                <option key={position} value={position}>
+                  {position}
+                </option>
+              ))}
+            </Select>
+            <Select
+              value={selectedJoinedTeam}
+              onChange={(e) => setSelectedJoinedTeam(e.target.value)}
+            >
+              <option value={""}>모든 사람</option>
+              <option value={"selected"}>선택된 사람</option>
+              <option value={"not-selected"}>선택되지 않은 사람</option>
+            </Select>
+            <Select
+              value={selectedRound}
+              onChange={(e) => setSelectedRound(+e.target.value)}
+            >
+              <option value={-1}>모든 지망</option>
+              {Array.from({ length: maxRound }).map((_, i) => (
+                <option key={i} value={i}>
+                  {i + 1}번째 지망
+                </option>
+              ))}
+            </Select>
+          </HStack>
+
+          <Heading size="md" margin="16px 0">
+            유저 목록
+          </Heading>
+
+          <HStack flexWrap={"wrap"}>
+            {filteredUsers.map((user) => (
+              <UserCard
+                key={user.id}
+                user={user}
+                teamId={selectedTeamId}
+                onPick={() => pick(user.id)}
+                onUnpick={() => unpick(user.id)}
+              />
+            ))}
+          </HStack>
+        </Box>
+
+        <Box width={"300px"} padding={2}>
+          <Select
+            value={selectedTeamId}
+            onChange={(e) => setSelectedTeamId(e.target.value)}
+          >
+            <option value={""}>팀 선택</option>
+            {teams.map((team) => (
+              <option key={team.id} value={team.id}>
+                {team.name}
+              </option>
+            ))}
+          </Select>
+          <Heading size="md" margin="16px 0">
+            선택한 팀원
+          </Heading>
+          <HStack flexWrap={"wrap"}>
+            {teamMembers.map((user) => (
+              <UserCard
+                key={user.id}
+                user={user}
+                teamId={selectedTeamId}
+                onPick={() => pick(user.id)}
+                onUnpick={() => unpick(user.id)}
+              />
+            ))}
+          </HStack>
+        </Box>
+      </HStack>
+
+      <Box padding={2}>
+        <Heading size="md">팀 빌딩 현황</Heading>
+        <Table size="sm">
+          <Thead>
+            <Tr>
+              <Th>팀 이름</Th>
+              <Th>멤버 이름</Th>
+              <Th>{`인원 수 (총 ${users.length}명)`}</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {allMemberByTeam.map(([teamName, users]) => (
+              <Tr key={teamName}>
+                <Td>{teamName}</Td>
+                <Td>
+                  {users
+                    .map((user) => `${user.name}/${user.position}`)
+                    .join(", ")}
+                </Td>
+                <Td>{users.length}명</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </>
+  );
+};
+
+type UserCardProps = {
+  user: User;
+  teamId?: Team["id"];
+  onPick?: () => void;
+  onUnpick?: () => void;
+};
+
+const UserCard = ({ user, teamId, onPick, onUnpick }: UserCardProps) => {
+  const picked = user.joined_team_id === teamId;
+
+  return (
+    <VStack
+      display={"inline-block"}
+      padding={"4px 8px"}
+      border={"1px solid gray"}
+      borderRadius={"4px"}
+    >
+      <Text>
+        {user.name} / {user.position}
+      </Text>
+      <Button
+        size="sm"
+        isDisabled={!teamId}
+        onClick={picked ? onUnpick : onPick}
+      >
+        {picked ? "unpick" : "pick"}
+      </Button>
+    </VStack>
+  );
+};
+
+const SetupBox = () => {
+  const {
+    users,
+    teams,
+    selectedTeamId,
+    selectedRound,
+    maxRound,
+    initByFile,
+    setSelectedJoinedTeam,
+    setSelectedRound,
+    setSelectedTeamId,
+  } = useGame();
+  const [started, setStarted] = useState(false);
+  const notInitialized = users.length === 0;
+
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const file = e.target.files[0];
+      initByFile(file);
+    }
+  };
+  const start = () => {
+    setSelectedTeamId(teams[0].id);
+    setSelectedRound(0);
+    setSelectedJoinedTeam("not-selected");
+
+    setStarted(true);
+  };
+  const next = () => {
+    const currIndex = teams.findIndex((team) => team.id === selectedTeamId);
+    const nextIndex = currIndex + 1;
+    if (nextIndex >= teams.length) {
+      const nextRound = selectedRound + 1;
+      if (nextRound === maxRound) {
+        // 끝
+        alert("끝");
+      } else {
+        // 다음 라운드
+        setSelectedTeamId(teams[0].id);
+        setSelectedRound(nextRound);
+      }
+    } else {
+      // 다음 팀으로
+      setSelectedTeamId(teams[nextIndex].id);
+    }
+  };
+
+  return (
+    <HStack padding="8px">
+      <Input type="file" accept=".tsv" onChange={handleFile} />
+      <Button isDisabled={notInitialized}>새 유저 추가</Button>
+      <Button isDisabled={notInitialized} onClick={started ? next : start}>
+        {started ? "다음" : "시작하기"}
+      </Button>
+    </HStack>
+  );
+};
--- src/store/useGame.tsx
+++ src/store/useGame.tsx
@@ -1,44 +1,143 @@
 import { create } from "zustand";
 
-type User = {
+export type User = {
   id: string;
   name: string;
   position: string;
   choices: Team["id"][];
   joined_team_id?: string | null;
 };
-type Team = {
+export type Team = {
   id: string;
   name: string;
 };
-
-type UseGame = {
+export type UseGame = {
   users: User[];
+  positions: User["position"][];
+  maxRound: number;
   teams: Team[];
-  round: number;
+  selectedPosition: User["position"];
+  selectedJoinedTeam: string;
+  selectedRound: number;
+  selectedTeamId: Team["id"];
+  initByFile: (file: File) => void;
   setUsers: (users: User[]) => void;
+  setTeams: (teams: Team[]) => void;
+  setSelectedTeamId: (teamId: Team["id"]) => void;
+  setSelectedPosition: (position: User["position"]) => void;
+  setSelectedJoinedTeam: (joinedTeam: string) => void;
+  setSelectedRound: (round: number) => void;
+  pick: (userId: User["id"]) => void;
+  unpick: (userId: User["id"]) => void;
 };
 
-export const useGame = create((set, get) => ({
-  users: [] as User[],
-  teams: [] as Team[],
-  round: 1,
+export const useGame = create<UseGame>((set, get) => ({
+  users: [],
+  positions: [],
+  maxRound: 0,
+  teams: [],
+  selectedTeamId: "",
+  selectedPosition: "",
+  selectedJoinedTeam: "",
+  selectedRound: -1,
+  initByFile: (file: File) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const { result } = reader;
+      const [, ...content] = (result as string).split("\r\n");
+      const users: User[] = [];
+      const choiceSet = new Set<string>();
+
+      const trim = (str: string) => {
+        return str.trim().replace(/\s+/g, " ").replace(/"/g, "");
+      };
+      const sortUser = (a: User, b: User) => {
+        if (a.position === b.position) {
+          return a.name.localeCompare(b.name);
+        }
+        return a.position.localeCompare(b.position);
+      };
+
+      content.forEach((line) => {
+        const [name, position, ...choices] = line.split("\t");
+        const trimmedChoices = choices.map(trim);
+
+        users.push({
+          id: Math.random().toString(36).substr(2, 9),
+          name,
+          position: trim(position),
+          choices: trimmedChoices,
+          joined_team_id: null,
+        });
+        trimmedChoices.forEach((choice) => choiceSet.add(choice));
+      });
+      const teams: Team[] = [...choiceSet]
+        .map((choice) => ({
+          id: choice,
+          name: choice,
+        }))
+        .sort((a, b) => a.name.localeCompare(b.name));
+
+      const positions = [...new Set(users.map((user) => user.position))].sort(
+        (a, b) => a.localeCompare(b)
+      );
+      const maxRound = users
+        .map((user) => user.choices.length)
+        .reduce((a, b) => Math.max(a, b), 0);
+
+      set({
+        users: users.sort(sortUser),
+        positions,
+        maxRound,
+        teams,
+      });
+    };
+    reader.readAsText(file);
+  },
   setUsers: (users: User[]) => {
-    set({ users });
+    const positions = [...new Set(users.map((user) => user.position))].sort(
+      (a, b) => a.localeCompare(b)
+    );
+    const maxRound = users
+      .map((user) => user.choices.length)
+      .reduce((a, b) => Math.max(a, b), 0);
+
+    set({ users, positions, maxRound });
   },
   setTeams: (teams: Team[]) => {
     set({ teams });
   },
-  setRound: (round: number) => {
-    set({ round });
+  setSelectedTeamId: (teamId: Team["id"]) => {
+    set({ selectedTeamId: teamId });
   },
-  pick: (teamId: Team["id"], userIds: User["id"][]) => {
-    const teams = get().team;
-    //
-    // set({ users })
+  setSelectedPosition: (position: User["position"]) => {
+    set({ selectedPosition: position });
   },
-  unpick: (teamId: Team["id"], userIds: User["id"][]) => {
-    //
-    // set({ users })
+  setSelectedJoinedTeam: (joinedTeam: string) => {
+    set({ selectedJoinedTeam: joinedTeam });
+  },
+  setSelectedRound: (round: number) => {
+    set({ selectedRound: round });
+  },
+  pick: (userId: User["id"]) => {
+    const { users, selectedTeamId } = get();
+    const user = users.find((user) => user.id === userId);
+
+    if (user) {
+      user.joined_team_id = selectedTeamId;
+      set({ users: users.slice() });
+    }
+  },
+  unpick: (userId: User["id"]) => {
+    const users = get().users;
+    const user = users.find((user) => user.id === userId);
+
+    if (user) {
+      user.joined_team_id = null;
+      set({ users: users.slice() });
+    }
   },
 }));
+
+useGame.subscribe(console.log);
